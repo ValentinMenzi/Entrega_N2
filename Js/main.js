39de28e90815cf87221e9d3a280cd205a784352d
Valentin Menzi <menzivalentin@gmail.com>
2023-07-10 20:06:17 -0300

    commit

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,32 @@
-let genero = prompt("Ingrese su Genero con M para Masculino o F para Femenino ");
-let edad = parseFloat(prompt("Ingrese su edad"));
+const Usuario = {
+  genero: "",
+  edad: 0,
+  altura: 0,
+  peso: 0,
+  nombre: "",
+  dni: "",
+  obraSocialNombre: "",
+  obraSocialNumero: "",
+  dia: "",
+  imc: 0
+};
+
+const doctores = [
+  "Dr. Juan Pérez",
+  "Dra. Ana Gómez",
+  "Dr. Carlos Rodríguez",
+  "Dra. Laura Martínez",
+  "Dr. Alejandro López"
+];
+
+// Función para seleccionar un doctor aleatorio del array
+function seleccionarDoctorAleatorio() {
+  const indiceAleatorio = Math.floor(Math.random() * doctores.length);
+  return doctores[indiceAleatorio];
+}
+
+Usuario.genero = prompt("Ingrese su género con M para Masculino o F para Femenino");
+Usuario.edad = parseFloat(prompt("Ingrese su edad"));
 
 let resumen = document.getElementsByClassName("resumen")[0];
 let respuesta = document.getElementsByClassName("respuesta")[0];
@@ -11,11 +38,11 @@ let diaElement = document.getElementsByClassName("dia")[0];
 
 let imc = 0;
 
-if ((genero === "M" || genero === "m") && edad > 19) {
-  let altura = parseFloat(prompt("Ingrese su altura con el signo ( . )"));
-  let peso = parseFloat(prompt("Ingrese su peso"));
+if ((Usuario.genero === "M" || Usuario.genero === "m") && Usuario.edad > 19) {
+  Usuario.altura = parseFloat(prompt("Ingrese su altura con el signo ( . )"));
+  Usuario.peso = parseFloat(prompt("Ingrese su peso"));
 
-  imc = peso / (altura * altura);
+  imc = Usuario.peso / (Usuario.altura * Usuario.altura);
 
   if (imc <= 19) {
     resumen.innerHTML = "Usted tiene bajo peso, le recomendamos comunicarse con nuestro nutricionista";
@@ -28,11 +55,11 @@ if ((genero === "M" || genero === "m") && edad > 19) {
   } else {
     resumen.innerHTML = "Usted tiene Obesidad de un grado muy alto, comuníquese con urgencia con nuestros doctores.";
   }
-} else if ((genero === "F" || genero === "f") && edad > 19) {
-  let altura = parseFloat(prompt("Ingrese su altura con el signo ( . )"));
-  let peso = parseFloat(prompt("Ingrese su peso"));
+} else if ((Usuario.genero === "F" || Usuario.genero === "f") && Usuario.edad > 19) {
+  Usuario.altura = parseFloat(prompt("Ingrese su altura con el signo ( . )"));
+  Usuario.peso = parseFloat(prompt("Ingrese su peso"));
 
-  imc = peso / (altura * altura);
+  imc = Usuario.peso / (Usuario.altura * Usuario.altura);
 
   if (imc <= 17) {
     resumen.innerHTML = "Usted tiene bajo peso, le recomendamos comunicarse con nuestro nutricionista.";
@@ -53,25 +80,19 @@ if (imc > 30 || imc < 19) {
   let respuestaUsuario = prompt("Usted esta teniendo porblemas con su peso ¿Le gustaría atenderse con uno de nuestros médicos? (si o no)");
 
   if (respuestaUsuario.toLowerCase() === "si") {
-    let nombre = prompt("Ingrese su Nombre");
-    let dni = prompt("Ingrese su DNI");
-    let obraSocialNombre = prompt("Ingrese el nombre de su obra social");
-    let obraSocialNumero = prompt("Ingrese el número de su obra social");
-    let dia = prompt("Ingrese el día que le gustaría asistir (dd/mm)");
-    const min = 8;
-    const max = 21;
-    const min1 = 0;
-    const max1 = 59;
-    const hora = Math.floor(Math.random() * (max - min + 1)) + min;
-    const hora2 = Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
+    Usuario.nombre = prompt("Ingrese su Nombre");
+    Usuario.dni = prompt("Ingrese su DNI");
+    Usuario.obraSocialNombre = prompt("Ingrese el nombre de su obra social");
+    Usuario.obraSocialNumero = prompt("Ingrese el número de su obra social");
+    Usuario.dia = prompt("Ingrese el día que le gustaría asistir (dd/mm)");
 
-    nombreElement.textContent = `Gracias ${nombre} sus datos fueron tomados`;
-    dniElement.textContent = `Su DNI: ${dni}`;
-    obraSocialNombreElement.textContent = `Su obra social: ${obraSocialNombre}`;
-    obraSocialNumeroElement.textContent = `El número de la obra social: ${obraSocialNumero}`;
-    diaElement.textContent = `Su turno es el día: ${dia}/2022 a las ${hora}:${hora2}hs`;
-  } else {
+    let doctor = seleccionarDoctorAleatorio();
+
+    nombreElement.textContent = `Gracias ${Usuario.nombre} sus datos fueron tomados`;
+    dniElement.textContent = `Su DNI: ${Usuario.dni}`;
+    obraSocialNombreElement.textContent = `Su obra social: ${Usuario.obraSocialNombre}`;
+    obraSocialNumeroElement.textContent = `El número de la obra social: ${Usuario.obraSocialNumero}`;
+    diaElement.textContent = `Su turno es el día: ${Usuario.dia}/2023 con el doctor ${doctor}`;
     respuesta.innerHTML = "Gracias por elegirnos";
   }
 }
-
